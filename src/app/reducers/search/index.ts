a98e88e69f05e5cca4b84884aee216633e758272
Matías Lobo <matiasnl.1993@gmail.com>
2020-08-29 02:11:46 -0300

    Creación de selectores

---
+++ src/app/reducers/search/index.ts
@@ -0,0 +1,20 @@
+import { AppState } from '../../app.reducers';
+import { createSelector } from '@ngrx/store';
+import * as search from './search.reducer';
+
+export const selectSearchFeature = (state: AppState) => state.search;
+
+export const getQuery = createSelector(
+    selectSearchFeature,
+    (state: search.State) => state.query
+);
+
+export const getType = createSelector(
+    selectSearchFeature,
+    (state: search.State) => state.type
+);
+
+export const getResults = createSelector(
+    selectSearchFeature,
+    (state: search.State) => state.results
+);
